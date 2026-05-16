8edbc664fde0d8ab0faa391c44bc430e1ef90aa2
BallisticEel <97997886+gilesshaxted@users.noreply.github.com>
2026-05-16 11:05:22 +0100

    Create firebase.js

---
+++ utils/firebase.js
@@ -0,0 +1,18 @@
+// utils/firebase.js
+import admin from 'firebase-admin';
+import { readFileSync } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const serviceAccountPath = path.join(__dirname, '../serviceAccountKey.json');
+
+const serviceAccount = JSON.parse(readFileSync(serviceAccountPath, 'utf8'));
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
+}
+
+export const db = admin.firestore();
